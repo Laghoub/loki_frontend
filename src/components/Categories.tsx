0daad34490f45db37487ac7b1313776eaef6989c
Laghoub <hn_laghoub@esi.dz>
2023-09-30 02:08:44 +0200

    Add category api

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState, ReactNode, useEffect } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -6,6 +7,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import axios from "axios";
+import configData from "../config.json";
 
 function createData(name: string) {
   return { name };
@@ -20,6 +23,31 @@ const rows = [
 ];
 
 export default function Categorie() {
+  type CategorieType = {
+    nom: string;
+  };
+
+  const [categories, setCategories] = useState([] as CategorieType[]);
+  const [loading, setLoading] = useState(true);
+  const SERVER_URL = configData.SERVER_URL;
+
+  useEffect(() => {
+    axios
+      .get(`${SERVER_URL}/products/category`)
+      .then((response) => {
+        setCategories(response.data);
+        setLoading(false); // Set loading to false once data is loaded
+      })
+      .catch((error) => {
+        console.error("Error fetching categories data:", error);
+        setLoading(false); // Set loading to false in case of an error
+      });
+  });
+
+  // Conditional rendering: Show loading indicator while loading
+  if (loading) {
+    return <p>Loading...</p>;
+  }
   return (
     <TableContainer component={Paper}>
       <Table sx={{}} aria-label="simple table">
@@ -31,13 +59,13 @@ export default function Categorie() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {categories.map((categorie) => (
             <TableRow
-              key={row.name}
+              key={categorie.nom}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {categorie.nom}
               </TableCell>
             </TableRow>
           ))}
